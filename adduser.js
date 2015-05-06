94f8b1ae64f1a0b4940a226f9a303324c2faf502
Lars T Hansen <lth@acm.org>
2015-05-06 09:07:29 +0200

    WIP

--- adduser.js
+++ adduser.js
@@ -1,12 +1,18 @@
 #!/usr/bin/nodejs
-const fs = require('fs');
 
+// Add a user to the user database.  Run in the directory above the
+// data directory.
+//
+// usage: adduser.js username password
+//
+// TODO: not terribly safe to specify the password on the command line,
+// it's better to read it from the console.
+
+const fs = require('fs');
 const args = process.argv.slice(2);
 
-if (args.length < 2) {
-    console.log("Usage: adduser username password");
-    process.exit(1);
-}
+if (args.length != 2)
+    fail("Usage: adduser username password");
 
 var userdb;
 
@@ -14,35 +20,41 @@ try {
     userdb = JSON.parse(fs.readFileSync("data/users.json"));
 }
 catch (e) {
-    console.log("Unable to read or parse user database");
-    console.log("Error is:");
-    console.log(e);
-    process.exit(1);
+    fail("Unable to read or parse user database\n" +
+	 "Error is:\n" +
+	 e);
 }
 
-if (!("version" in userdb) || userdb.version > 1) {
-    console.log("User database has unsupported version: " + userdb.version);
-    process.exit(1);
-}
+if (!("version" in userdb) || userdb.version > 1)
+    fail("User database has unsupported version: " + userdb.version);
 
 const username = args[0];
 const password = args[1];
 
-// TODO: vet the username: [A-Za-z0-9]+
-// TODO: vet the password: not empty
+if (!username.match(/^[A-Za-z0-9]+$/))
+    fail("Bad user name, must match [A-Za-z0-9]+");
+
+if (password == "")
+    fail("Empty password");
 
 for ( var i=0 ; i < userdb.users.length ; i++ ) {
     var u = userdb.users[i];
-    if (u.user == username) {
-	// TODO: This could be a password update, but only if there's
-	// an explicit option to support that
-	console.log("Duplicate user");
-	process.exit(1);
-    }
+    // TODO: A duplicate user could be a password update, but only if there's
+    // an explicit option to support that, and we don't have that
+    if (u.user == username)
+	fail("Duplicate user " + username);
 }
 
 userdb.users.push({user: username, passwd: password});
 
-// TODO: this races with any running server.
+// TODO: this races with any server that's just starting up.
+// TODO: this races with other runs of adduser.js
+// TODO: if there's a running server then it needs to re-read the users
+//       after we add one.
 
 fs.writeFileSync("data/users.json", JSON.stringify(userdb));
+
+function fail(msg) {
+    console.log(msg);
+    process.exit(1);
+}
